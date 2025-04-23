aeee6dbfdbbdeffce78ed267a541052e288314d7
Rtsmks <aquilafavolia@gmail.com>
2025-04-23 21:28:10 +0700

    implement splitToArr

--- compiled/main.js
+++ compiled/main.js
@@ -1,28 +1,18 @@
-export const splitToArr = (input?:string):(string[]|string)=>{
-
+export const splitToArr = (input) => {
     //your answer here
-
     //undefined
-    if (input === undefined){
+    if (input === undefined) {
         return "Invalid input";
     }
-
     //empty string
-    if (input.trim() === ""){
+    if (input.trim() === "") {
         return "Data not available";
     }
-
     //split string
     const cleanedInput = input.replaceAll(";", " ").replaceAll("-", " ");
     const splitNames = cleanedInput.split(" ");
-    const formattedNames = splitNames.map((name) => 
-        name.charAt(0).toUpperCase() + name.slice(1)
-    )
-    
-    return formattedNames
-
-}
-
+    const formattedNames = splitNames.map((name) => name.charAt(0).toUpperCase() + name.slice(1));
+    return formattedNames;
+};
 const result = splitToArr("shelda.alni;pahlevi.fikri-ricky.kurniawan");
 console.log(result);
-
